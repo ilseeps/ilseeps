63ec17b4b5b2e1474de598dc7597869c10c58a60
Ilse <97129051+ilseeps@users.noreply.github.com>
2022-01-10 09:47:28 +0100

    Update notebook_pass_data.py

--- AzureTutorials/adf_adb_pass_variables/notebook_pass_data.py
+++ AzureTutorials/adf_adb_pass_variables/notebook_pass_data.py
@@ -1,12 +1,10 @@
 import json
-
 inputv=dbutils.widgets.get("adf_input_value")
-
 if inputv=="1":
   outputv="2"
 else:
-  outputv="1" 
-  
+  outputv="1"
+
 dbutils.notebook.exit(json.dumps({
     "adf_output_value" : outputv
 }))
